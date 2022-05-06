8426407f55efbb6bdb68e3396261515ff21e9f86
Morampudi1 <morampudi.chenna90@gmail.com>
2022-05-06 16:09:41 +0530

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './chenna/main/main.component';
+import { HemaComponent } from './hemanth/hema/hema.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HemaComponent
   ],
   imports: [
     BrowserModule,
